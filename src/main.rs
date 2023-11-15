cfa9929b1f720e0cccb3073bf935689bf44b7d27
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2023-11-15 20:50:05 +0000

    adds bin crate to extract text from pdf files

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use std::path::PathBuf;
+use text_extractor::extract_text;
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Path to the file to extract text from 
+    #[arg(short, long, value_name = "FILE")]
+    extract: Option<PathBuf>,
+
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if let Some(path) = cli.extract.as_deref() {
+        match extract_text(path.to_str().unwrap()) {
+            Ok(texts) => {
+                for text in texts {
+                    println!("{}", text);
+                }
+            },
+            Err(err) => {
+                println!("Error: {}", err);
+            }
+        }
+    }
+}
