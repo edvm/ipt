1c3c9879097d57bffd5e82e781bc8f9eab9f81df
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2023-11-15 20:00:27 +0000

    adds fn extract_text and extract_from_pdf. Test extract_text when given path has no file extension

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+use std::path::Path;
+
+use lopdf::Document;
+
+pub fn extract_from_pdf(path: &str) -> Result<Vec<String>, String> {
+
+    match Document::load(path) {
+        Ok(document) => {
+            let pages = document.get_pages();
+            let mut texts = Vec::new();
+
+            for (i, _) in pages.iter().enumerate() {
+                let page_number = (i + 1) as u32;
+                let text = document.extract_text(&[page_number]);
+                texts.push(text.unwrap_or_default());
+            }
+
+            Ok(texts)
+        },
+        Err(err) => {
+            Err(err.to_string())
+        }
+    }
+
+}
+
+pub fn extract_text(path: &str) -> Result<Vec<String>, String> {
+    let path = Path::new(path);
+    match path.extension() {
+        Some(ext) => {
+            match ext.to_str() {
+                Some("pdf") => extract_from_pdf(path.to_str().unwrap()),
+                _ => {
+                    let err = format!("Unsupported file extension: {}", ext.to_str().unwrap());
+                    Err(err)
+                }
+            }
+        },
+        None => {
+            let err = format!("File has no extension: {}", path.to_str().unwrap());
+            Err(err)
+        }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -7,8 +52,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn no_file_extension() {
+        extract_text("path/to/file").expect_err("File has no extension: path/to/file");
     }
 }
